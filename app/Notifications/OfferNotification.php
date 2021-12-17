5a19e20078dcb6212760618e31dc825efec0dfb3
JastiDev <sujaneshdev@gmail.com>
2021-12-17 15:37:13 -0500

    testing on notification

--- app/Notifications/OfferNotification.php
+++ app/Notifications/OfferNotification.php
@@ -59,7 +59,7 @@ class OfferNotification extends Notification
             'message' => $this->offer['message'],
             'url' => $this->offer['url'] ?? null,
             'url_text' => $this->offer['url_text'] ?? null,
-            'image_url' => $this->data['image_url'] ?? null
+            'image_url' => $this->data['image_url']
         ];
     }
 }
