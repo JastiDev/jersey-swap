1f5a5e68cb2556a6db8abc2cb215fce7398d3378
JastiDev <sujaneshdev@gmail.com>
2022-01-10 17:26:54 -0500

    NNN added

--- resources/views/frontend/pages/messages.blade.php
+++ resources/views/frontend/pages/messages.blade.php
@@ -190,7 +190,8 @@
       $(".user-list").removeClass("close-list");
     })
 
-    let ws = new WebSocket('wss://174.129.151.80:8091');
+    // let ws = new WebSocket('wss://174.129.151.80:8091');
+    var ws = new WebSocket("wss://174.129.151.80/wss2/NNN");
     // var serverIp =  {!! env('SERVER_IP', '174.129.151.80') !!};
     // let ws = new WebSocket('ws://localhost:8090');
   
