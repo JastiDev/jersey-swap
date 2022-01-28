3e621e191dd7ea3e4852543763961845208e6e5e
JastiDev <sujaneshdev@gmail.com>
2022-01-28 15:36:52 -0500

    postal code in dashboard

--- resources/views/frontend/pages/users/dashboard.blade.php
+++ resources/views/frontend/pages/users/dashboard.blade.php
@@ -91,7 +91,7 @@
                                 <hr>
                                 <div class="row">
                                     <div class="col-sm-3">
-                                        <h6 class="mb-0">Post Code</h6>
+                                        <h6 class="mb-0">Postal Code</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
                                         {{auth()->user()->postcode}}
