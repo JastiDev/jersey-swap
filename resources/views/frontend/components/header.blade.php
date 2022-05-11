b9c059be86ab457cbd6febdde066731825e9f285
Alexander S <alexander@sharapov.biz>
2022-05-11 22:40:21 +0600

    Updating gtag

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -42,12 +42,14 @@
         var static_url = "{{static_url('')}}";
     </script>
 
-    <!-- Gtag -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-228480313-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'UA-228181667-1');
+
+        gtag('config', 'UA-228480313-1');
     </script>
 </head>
 <body id="body">
